5ffeaf61c30226db08e17b037beed0071aee13fc
Sam King <mail@samking.co>
2019-04-28 20:04:21 +0100

    Add header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from '@reach/router';
-import { Box, Headline, Caption } from '../design-system';
+import { Box, Headline, Caption, Link } from '../design-system';
 
 function Header() {
   return (
-    <Box as="header" role="banner">
-      <Headline as="h1">
-        <Link to="/">Sam King</Link>
+    <Box as="header" role="banner" pt={[1, 2]} px={[1, 2]} mb={5}>
+      <Headline as="h1" fontSize={1} letterSpacing="sparse" mb={1}>
+        <Link to="/" shouldUnderline={false}>
+          Sam King
+        </Link>
       </Headline>
-      <Caption as="h2">Product Designer for Restaurants at Deliveroo</Caption>
+      <Caption as="h2" color="textAlt">
+        Product Designer for Restaurants at Deliveroo
+      </Caption>
     </Box>
   );
 }
--- src/design-system/Box.js
+++ src/design-system/Box.js
@@ -1,10 +1,20 @@
 import styled from '@emotion/styled';
 import shouldForwardProp from '@styled-system/should-forward-prop';
-import { space, color, width } from 'styled-system';
+import {
+  space,
+  color,
+  width,
+  position,
+  top,
+  left,
+  right,
+  bottom,
+  zIndex,
+} from 'styled-system';
 
 const Box = styled('div', {
   shouldForwardProp,
-})(space, width, color);
+})(space, width, color, position, top, left, right, bottom, zIndex);
 
 Box.displayName = 'Box';
 
