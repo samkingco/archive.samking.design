16be3b866a5d0649e109aef64019e4a3a01ffc22
Sam King <mail@samking.co>
2019-04-30 22:00:02 +0100

    Fix style of figcaptions

--- src/design-system/Figure.js
+++ src/design-system/Figure.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Box from './Box';
 import Image from './Image';
+import Caption from './Caption';
 
-const Figure = ({ src, alt, caption, ratio, ...props }) => (
-  <Box as="figure" {...props}>
-    <Image src={src} alt={alt} ratio={ratio} />
-    {caption && <Box as="figcaption">{caption}</Box>}
-  </Box>
-);
+const Figure = styled(
+  ({ src, srcSet, sizes, alt, caption, ratio, ...props }) => (
+    <Box as="figure" {...props}>
+      <Image src={src} srcSet={srcSet} sizes={sizes} alt={alt} ratio={ratio} />
+      {caption && (
+        <Caption as="figcaption" color="textAlt" textAlign="right">
+          {caption}
+        </Caption>
+      )}
+    </Box>
+  ),
+)();
+
+Figure.defaultProps = {
+  m: 0,
+};
 
 Figure.displayName = 'Figure';
 
