5a9c14e13df82a9016b4d6369ba0368bfad6c01e
Sam King <mail@samking.co>
2019-04-30 22:03:54 +0100

    Style job

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Box, Headline, Body } from '../design-system';
+import { Box, Headline, Body, Caption } from '../design-system';
 
 function Job({ date, title, company, about, ...props }) {
   return (
     <Box {...props}>
-      <Body aria-label="Date:">{date}</Body>
-      <Headline as="h3">
+      <Caption aria-label="Date:" color="textAlt" mb={1} display="block">
+        {date}
+      </Caption>
+      <Headline as="h3" fontSize={1} mb={1}>
         {`${title}${company ? ` at ${company}` : ''}`}
       </Headline>
       <Body>{about}</Body>
