c0ece094e7a214152f9d36c1420f003309ded7de
Sam King <mail@samking.co>
2019-04-30 22:03:34 +0100

    Style project preview

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import { Box, Headline, Body } from '../design-system';
+import { Box, Headline, Body, Grid, Link } from '../design-system';
 
-function ProjectPreview({ title, intro, date, myRole, ...props }) {
+function ProjectPreview({ slug, title, intro, date, myRole, ...props }) {
   return (
-    <Box {...props}>
-      <Headline as="h3" aria-label={`Project name: ${title}`}>
-        {title}
-      </Headline>
-      <Body aria-label="About:">{intro}</Body>
-      <Body aria-label="Date:">{date}</Body>
-      <Body aria-label="My role:">{myRole}</Body>
-    </Box>
+    <Link
+      to={`/${slug}`}
+      {...props}
+      display="block"
+      shouldUnderline={false}
+      border={2}
+      borderColor="fg"
+    >
+      <Box borderBottom={2} borderColor="fg" p={2}>
+        <Headline as="h3" aria-label={`Project name: ${title}`} mb={1}>
+          {title}
+        </Headline>
+        <Body aria-label="About:">{intro}</Body>
+      </Box>
+
+      <Grid gridTemplateColumns="repeat(6, 1fr)">
+        <Body
+          aria-label="Date:"
+          gridColumn={['1 / span 6', null, '1 / span 2']}
+          py={1}
+          px={2}
+          borderColor="fg"
+        >
+          {date}
+        </Body>
+        <Body
+          aria-label="My role:"
+          gridColumn={['1 / span 6', null, '3 / span 4']}
+          py={1}
+          px={2}
+          borderTop={[2, null, 0]}
+          borderLeft={[0, null, 2]}
+          borderColor={['fg', null, 'fg']}
+        >
+          {myRole}
+        </Body>
+      </Grid>
+    </Link>
   );
 }
 
