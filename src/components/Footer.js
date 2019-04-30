eee7af7e3c358b86fd01d01d5db83d2e11006dc3
Sam King <mail@samking.co>
2019-04-30 22:02:27 +0100

    Update Footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,10 @@ function Header() {
   const year = new Date().getFullYear();
 
   return (
-    <Box as="footer" role="contentinfo">
-      <Caption>Content &copy; {year} Sam King</Caption>
+    <Box as="footer" role="contentinfo" px={[1, 2]} pb={[1, 2]} mt={5}>
+      <Caption display="block" textAlign="right" color="textAlt">
+        Content &copy; {year} Sam King
+      </Caption>
     </Box>
   );
 }
