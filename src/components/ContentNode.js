2aa80f9bbba8557fbe545f53af7db580afdfb6f8
Sam King <mail@samking.co>
2019-04-30 22:49:25 +0100

    Style project page

--- src/components/ContentNode.js
+++ src/components/ContentNode.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Headline, Body, Figure } from '../design-system';
 
-const ContentNode = ({ node }) => {
+const ContentNode = ({ node, ...props }) => {
   const type = Array.isArray(node) ? node[0] : null;
   const children = Array.isArray(node) ? node[1] : node;
   const attributes = Array.isArray(node) ? node[2] : null;
@@ -9,7 +8,7 @@ const ContentNode = ({ node }) => {
   const nodeChildren = Array.isArray(children) ? (
     <React.Fragment>
       {children.map((node, index) => (
-        <ContentNode key={index} node={node} />
+        <ContentNode key={index} node={node} {...props} />
       ))}
     </React.Fragment>
   ) : (
@@ -22,30 +21,38 @@ const ContentNode = ({ node }) => {
 
   switch (type) {
     case 'h1':
-      return <Headline as="h1">{nodeChildren}</Headline>;
+      return props.h1 ? props.h1({ children: nodeChildren }) : null;
     case 'h2':
-      return <Headline as="h2">{nodeChildren}</Headline>;
+      return props.h2 ? props.h2({ children: nodeChildren }) : null;
     case 'h3':
-      return <Headline as="h3">{nodeChildren}</Headline>;
+      return props.h3 ? props.h3({ children: nodeChildren }) : null;
     case 'h4':
-      return <Headline as="h4">{nodeChildren}</Headline>;
+      return props.h4 ? props.h4({ children: nodeChildren }) : null;
     case 'h5':
-      return <Headline as="h5">{nodeChildren}</Headline>;
+      return props.h5 ? props.h5({ children: nodeChildren }) : null;
     case 'h6':
-      return <Headline as="h6">{nodeChildren}</Headline>;
+      return props.h6 ? props.h6({ children: nodeChildren }) : null;
     case 'p':
-      return <Body>{nodeChildren}</Body>;
+      return props.p ? props.p({ children: nodeChildren }) : null;
     case 'a':
-      return attributes ? <a href={attributes.href}>{nodeChildren}</a> : null;
+      return attributes && props.a
+        ? props.a({
+            children: nodeChildren,
+            href: attributes.href,
+            title: attributes.title,
+          })
+        : null;
     case 'img':
-      return attributes ? (
-        <Figure
-          src={attributes.src}
-          alt={attributes.alt}
-          caption={attributes.caption}
-          ratio={attributes.ratio}
-        />
-      ) : null;
+      return attributes && props.img
+        ? props.img({
+            src: attributes.src,
+            alt: attributes.alt,
+            caption: attributes.caption,
+            width: attributes.width,
+            height: attributes.height,
+            ratio: attributes.ratio,
+          })
+        : null;
     default:
       return null;
   }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,22 +1,109 @@
 import React from 'react';
 import { useRouteData } from 'react-static';
-import { Box, Title, Body } from '../design-system';
+import styled from '@emotion/styled';
+import {
+  Box,
+  Title,
+  Body,
+  Figure,
+  Headline,
+  Link,
+  Grid,
+  Caption,
+} from '../design-system';
 import ContentNode from '../components/ContentNode';
 
+const Wrapper = styled(Box)`
+  ${Headline} + ${Body},
+  ${Body} + ${Body} {
+    margin-top: ${props => props.theme.space[2]}px
+  }
+
+  ${Body} + ${Headline} {
+    margin-top: ${props => props.theme.space[4]}px
+  }
+
+  ${Figure} + ${Headline},
+  ${Figure} + ${Body},
+  ${Headline} + ${Figure},
+  ${Body} + ${Figure} {
+    margin-top: ${props => props.theme.space[5]}px
+  }
+`;
+
 function Project() {
-  const { title, intro, content } = useRouteData();
+  const { title, intro, date, myRole, cover, content } = useRouteData();
+
+  const textGridColumn = [
+    '1 / span 8',
+    '2 / span 7',
+    '2 / span 6',
+    '2 / span 5',
+    '3 / span 4',
+  ];
 
   return (
-    <Box as="article">
-      <Box as="hgroup">
-        <Title>{title}</Title>
-        <Body>{intro}</Body>
-      </Box>
+    <Wrapper as="main" role="main" px={[1, 2]} maxWidth="70em" mx="auto">
+      <Grid as="article" gridTemplateColumns="repeat(8, 1fr)">
+        <Box as="hgroup" gridColumn={textGridColumn} mb={5}>
+          <Title mb={3}>{title}</Title>
+          <Body mb={1}>{intro}</Body>
+          <Caption color="textAlt">
+            {date} • {myRole}
+          </Caption>
+        </Box>
 
-      {content.map((node, index) => (
-        <ContentNode key={`${node.type}.${index}`} node={node} />
-      ))}
-    </Box>
+        <Figure
+          src={cover.src}
+          alt={cover.alt}
+          ratio={cover.ratio}
+          gridColumn={['1 / span 8', null, '2 / span 7']}
+          mr={[-1, -2]}
+          ml={[-1, -2, 0]}
+        />
+
+        {content.map((node, index) => (
+          <ContentNode
+            key={`${node.type}.${index}`}
+            node={node}
+            h1={({ children }) => (
+              <Headline as="h1" gridColumn={textGridColumn}>
+                {children}
+              </Headline>
+            )}
+            h2={({ children }) => (
+              <Headline as="h2" gridColumn={textGridColumn}>
+                {children}
+              </Headline>
+            )}
+            h3={({ children }) => (
+              <Headline as="h3" gridColumn={textGridColumn}>
+                {children}
+              </Headline>
+            )}
+            p={({ children }) => (
+              <Body gridColumn={textGridColumn}>{children}</Body>
+            )}
+            a={({ children, href }) => <Link to={href}>{children}</Link>}
+            img={({ src, alt, caption, ratio }) => (
+              <Figure
+                src={src}
+                alt={alt}
+                caption={caption}
+                ratio={ratio}
+                gridColumn={[
+                  '1 / span 8',
+                  null,
+                  '2 / span 7',
+                  null,
+                  '2 / span 6',
+                ]}
+              />
+            )}
+          />
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
