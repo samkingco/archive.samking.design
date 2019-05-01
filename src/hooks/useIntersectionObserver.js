7f5a96e4acbdf59c27268011fc0ca42d239d4832
Sam King <mail@samking.co>
2019-05-01 09:44:52 +0100

    Fix intersection observer when buiilding for prod

--- src/hooks/useIntersectionObserver.js
+++ src/hooks/useIntersectionObserver.js
@@ -1,30 +1,33 @@
 import { useState, useEffect } from 'react';
 
 function useIntersectionObserver(ref, { threshold, root, rootMargin }) {
+  const isBrowser = typeof document !== 'undefined';
   const [state, setState] = useState({
     isInView: false,
     hasTriggered: false,
     entry: undefined,
   });
 
-  const observer = new IntersectionObserver(
-    (entries, observerInstance) => {
-      if (entries[0].intersectionRatio > 0) {
-        setState({
-          isInView: true,
-          hasTriggered: true,
-          entry: observerInstance,
-        });
-        observerInstance.unobserve(ref.current);
-      }
-      return;
-    },
-    {
-      threshold: threshold || 0,
-      root: root || null,
-      rootMargin: rootMargin || '0%',
-    },
-  );
+  const observer = !isBrowser
+    ? {}
+    : new IntersectionObserver(
+        (entries, observerInstance) => {
+          if (entries[0].intersectionRatio > 0) {
+            setState({
+              isInView: true,
+              hasTriggered: true,
+              entry: observerInstance,
+            });
+            observerInstance.unobserve(ref.current);
+          }
+          return;
+        },
+        {
+          threshold: threshold || 0,
+          root: root || null,
+          rootMargin: rootMargin || '0%',
+        },
+      );
 
   useEffect(() => {
     if (ref.current && !state.hasTriggered) {
