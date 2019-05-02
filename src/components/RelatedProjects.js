52449b44a0b45e2190393985031e836f0b543143
Sam King <mail@samking.co>
2019-05-02 21:04:03 +0100

    Flatten related projects component structure

--- src/components/RelatedProjects.js
+++ src/components/RelatedProjects.js
@@ -1,26 +1,53 @@
 import React, { useState } from 'react';
-import { Grid } from '../design-system';
-import ProjectPreview from './ProjectPreview';
+import { Grid, Box, Headline, Body, Link } from '../design-system';
 
 function RelatedProjects({ projects, ...props }) {
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const getBorderColor = index => (hoveredIndex === index ? 'accent' : 'bgAlt');
 
   return (
     <Grid gridTemplateColumns="repeat(8, 1fr)" {...props}>
-      {projects.map((project, index) => (
-        <ProjectPreview
-          key={project.slug}
-          slug={project.slug}
-          title={project.title}
-          byline={project.byline}
-          date={project.date}
+      {projects.map(({ slug, title, byline, date }, index) => (
+        <Link
+          key={slug}
+          to={`/${slug}`}
           role="listitem"
+          shouldUnderline={false}
+          display="block"
           gridColumn={['1 / span 8', '2 / span 6', '3 / span 4']}
           mb={index === projects.length - 1 ? 0 : 4}
-          borderColor={hoveredIndex === index ? 'accent' : 'bgAlt'}
+          border={2}
+          borderColor={getBorderColor(index)}
           onMouseEnter={() => setHoveredIndex(index)}
           onMouseLeave={() => setHoveredIndex(null)}
-        />
+        >
+          <Box p={2}>
+            <Headline
+              as="h3"
+              aria-label={`Project name: ${title}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              {title}
+              <span aria-hidden="true"> →</span>
+            </Headline>
+
+            <Body aria-label="About:" mt={1}>
+              {byline}
+            </Body>
+          </Box>
+
+          <Grid
+            borderTop={2}
+            borderColor={getBorderColor(index)}
+            gridTemplateColumns="repeat(6, 1fr)"
+          >
+            <Body aria-label="Date:" gridColumn="1 / span 6" py={1} px={2}>
+              {date}
+            </Body>
+          </Grid>
+        </Link>
       ))}
     </Grid>
   );
