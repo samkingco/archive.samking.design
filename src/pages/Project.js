b4bd3333e8ccd87cee827f1c1548dd9e5512eec0
Sam King <mail@samking.co>
2019-05-02 23:21:19 +0100

    Update project layout

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -15,7 +15,7 @@ import { useTheme } from '../components/ActiveTheme';
 import ContentNode from '../components/ContentNode';
 import RelatedProjects from '../components/RelatedProjects';
 
-const Wrapper = styled(Box)`
+const ProjectContent = styled(Grid)`
   ${Headline} + ${Body},
   ${Body} + ${Body} {
     margin-top: ${props => props.theme.space[2]}px
@@ -40,7 +40,8 @@ function Project() {
     title,
     intro,
     date,
-    myRole,
+    roles,
+    link,
     cover,
     content,
     relatedProjects,
@@ -61,13 +62,30 @@ function Project() {
   ];
 
   return (
-    <Wrapper as="main" role="main" px={[1, 2]} py={5} maxWidth="70em" mx="auto">
+    <Box
+      as="main"
+      role="main"
+      px={[1, 2]}
+      py={[5, 6]}
+      maxWidth="70em"
+      mx="auto"
+    >
       <Grid as="article" gridTemplateColumns="repeat(8, 1fr)">
         <Box as="hgroup" gridColumn={textGridColumn} mb={5}>
-          <Title mb={3}>{title}</Title>
-          <Body mb={1}>{intro}</Body>
-          <Caption color="textAlt">
-            {date} • {myRole}
+          <Title mb={2}>{title}</Title>
+          <Body mb={3}>{intro}</Body>
+          <Caption as="p" color="textAlt">
+            <Box display={['block', null, 'inline-block']} mr={3}>
+              {date}
+            </Box>
+            <Box display={['block', null, 'inline-block']} mr={3}>
+              {roles.map(role => `${role}`).join(', ')}
+            </Box>
+            {link && (
+              <Box display={['block', null, 'inline-block']}>
+                <Link to={link.url}>{link.text}</Link>
+              </Box>
+            )}
           </Caption>
         </Box>
 
@@ -78,48 +96,54 @@ function Project() {
           gridColumn={['1 / span 8', null, '2 / span 7']}
           mr={[-1, -2]}
           ml={[-1, -2, 0]}
+          mb={5}
         />
 
-        {content.map((node, index) => (
-          <ContentNode
-            key={`${node.type}.${index}`}
-            node={node}
-            h1={({ children }) => (
-              <Headline as="h1" gridColumn={textGridColumn}>
-                {children}
-              </Headline>
-            )}
-            h2={({ children }) => (
-              <Headline as="h2" gridColumn={textGridColumn}>
-                {children}
-              </Headline>
-            )}
-            h3={({ children }) => (
-              <Headline as="h3" gridColumn={textGridColumn}>
-                {children}
-              </Headline>
-            )}
-            p={({ children }) => (
-              <Body gridColumn={textGridColumn}>{children}</Body>
-            )}
-            a={({ children, href }) => <Link to={href}>{children}</Link>}
-            img={({ src, alt, caption, ratio }) => (
-              <Figure
-                src={src}
-                alt={alt}
-                caption={caption}
-                ratio={ratio}
-                gridColumn={[
-                  '1 / span 8',
-                  null,
-                  '2 / span 7',
-                  null,
-                  '2 / span 6',
-                ]}
-              />
-            )}
-          />
-        ))}
+        <ProjectContent
+          gridColumn="1 / span 8"
+          gridTemplateColumns="repeat(8, 1fr)"
+        >
+          {content.map((node, index) => (
+            <ContentNode
+              key={`${node.type}.${index}`}
+              node={node}
+              h1={({ children }) => (
+                <Headline as="h1" gridColumn={textGridColumn}>
+                  {children}
+                </Headline>
+              )}
+              h2={({ children }) => (
+                <Headline as="h2" gridColumn={textGridColumn}>
+                  {children}
+                </Headline>
+              )}
+              h3={({ children }) => (
+                <Headline as="h3" gridColumn={textGridColumn}>
+                  {children}
+                </Headline>
+              )}
+              p={({ children }) => (
+                <Body gridColumn={textGridColumn}>{children}</Body>
+              )}
+              a={({ children, href }) => <Link to={href}>{children}</Link>}
+              img={({ src, alt, caption, ratio }) => (
+                <Figure
+                  src={src}
+                  alt={alt}
+                  caption={caption}
+                  ratio={ratio}
+                  gridColumn={[
+                    '1 / span 8',
+                    null,
+                    '2 / span 7',
+                    null,
+                    '2 / span 6',
+                  ]}
+                />
+              )}
+            />
+          ))}
+        </ProjectContent>
 
         <RelatedProjects
           projects={relatedProjects}
@@ -128,7 +152,7 @@ function Project() {
           mt={5}
         />
       </Grid>
-    </Wrapper>
+    </Box>
   );
 }
 
