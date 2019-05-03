08de882c561900d232fc74d533d686ccec4449fb
Sam King <mail@samking.co>
2019-05-03 22:46:58 +0100

    Add siteRoot property to config

--- static.config.js
+++ static.config.js
@@ -5,6 +5,7 @@ import history from './content/dist/history.json';
 export default {
   plugins: ['react-static-plugin-reach-router', 'react-static-plugin-emotion'],
   productionSourceMaps: true,
+  siteRoot: 'https://samking.design',
   getSiteData: async () => ({
     version: pkg.version,
     email: 'mail@samking.co',
