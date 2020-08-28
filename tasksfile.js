d56aa1fe4a8f3c56d174554a786d98e4b5a51264
Sam King <mail@samking.co>
2020-08-28 22:36:37 +0100

    Update dependencies

--- tasksfile.js
+++ tasksfile.js
@@ -34,33 +34,33 @@ function buildContent(_, stage = 'prod') {
     `APP_IMAGE_BASE_URLS=${IMAGE_BASE_URLS[stage]}`,
   ];
 
-  sh(`${env.join(' ')} node ${PATHS.bin}/build-content.js`);
+  sh(`cross-env ${env.join(' ')} node ${PATHS.bin}/build-content.js`);
 }
 
 function start() {
   console.log(`Starting ${pkg.name} for ${envMap.dev}`);
-  sh('react-static start', { async: true });
+  sh('cross-env react-static start', { async: true });
 }
 
 function buildSite(_, stage = 'prod') {
   console.log(`Building ${pkg.name} for ${envMap[stage]}`);
-  sh('react-static build');
+  sh('cross-env react-static build');
 }
 
 function analyze() {
   console.log(`Building ${pkg.name} for ${envMap.dev}`);
-  sh('react-static build --analyze');
+  sh('cross-env react-static build --analyze');
 }
 
 function serve(_, port = PORTS.SERVE_DIST) {
   console.log(`Serving ${pkg.name}`);
-  sh(`serve dist -p ${port}`, { async: true });
+  sh(`cross-env serve dist -p ${port}`, { async: true });
 }
 
 function serveImages(_, port = PORTS.IMAGE_SERVER) {
   console.log(`Starting ${pkg.name} mock image server`);
   const env = [`PORT=${port}`, `APP_IMAGES_DEST=${PATHS.imagesDest}`];
-  sh(`${env.join(' ')} nodemon ${PATHS.bin}/image-server.js`, { async: true });
+  sh(`cross-env ${env.join(' ')} nodemon ${PATHS.bin}/image-server.js`, { async: true });
 }
 
 help(buildContent, 'Builds content data for a particular environment.', {
